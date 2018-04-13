0ef30ef841eaa2e64e4a8d3dc6758ab9bec7f5a5
David Callu <callu.david@gmail.com>
2018-04-13 18:19:10 +0200

    chore(version): bump to 1.67.0-rc2

--- id.cmake
+++ id.cmake
@@ -1,17 +1,17 @@
 set(package Boost)
-set(version 1.66.0)
+set(version 1.67.0-rc2)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-4
+    VERSION ${version}-1
     ABI ${version}
     )
 
 byd__package__download_info(${package}
-    URL "https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2"
-    URL_HASH SHA1=b6b284acde2ad7ed49b44e856955d7b1ea4e9459
+    URL "https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0_rc2.tar.bz2"
+    URL_HASH SHA1=694ae3f4f899d1a80eb7a3b31b33be73c423c1ae
 #    GIT_REPOSITORY "https://github.com/boostorg/boost.git"
-#    GIT_TAG boost-1.66.0
+#    GIT_TAG boost-1.67.0-rc2
 #    GIT_CONFIG submodule.fetchJobs=${BYD__OPTION__JOBS}
     )
