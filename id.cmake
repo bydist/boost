ef7b7495f5562fb6ef69986d46ada7888ca305f0
David Callu <callu.david@gmail.com>
2019-04-12 14:14:45 +0200

    chore(id): bump to 1.70.0

--- id.cmake
+++ id.cmake
@@ -1,5 +1,5 @@
 set(package Boost)
-set(version  1.69.0)
+set(version  1.70.0)
 set(package_version  ${version})
 
 byd__package__info(${package}
@@ -10,9 +10,9 @@ byd__package__info(${package}
     )
 
 byd__package__download_info(${package}
-    URL "https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2"
-    URL_HASH SHA256=8f32d4617390d1c2d16f26a27ab60d97807b35440d45891fa340fc2648b04406
+    URL "https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.bz2"
+    URL_HASH SHA256=430ae8354789de4fd19ee52f3b1f739e1fba576f0aded0897c3c2bc00fb38778
 #    GIT_REPOSITORY "https://github.com/boostorg/boost.git"
-#    GIT_TAG boost-1.69.0
+#    GIT_TAG boost-1.70.0
 #    GIT_CONFIG submodule.fetchJobs=${BYD__OPTION__JOBS}
     )
